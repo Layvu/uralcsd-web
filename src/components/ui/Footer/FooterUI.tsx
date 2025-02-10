312c099732211b50de5327bdc1da2e331e411977
Layvu <zemskyalexey.writer@mail.ru>
2025-02-10 09:53:14 +0500

    feat: footer markup

--- src/components/ui/Footer/FooterUI.tsx
+++ src/components/ui/Footer/FooterUI.tsx
@@ -10,7 +10,7 @@ import { SvgIcon } from '@components/Shared/SvgIcon';
 export const FooterUI: React.FC = () => {
     return (
         <footer className="footer">
-            <div className="container footer__container">
+            <div className="wrap footer__wrap">
                 <div className="footer__left">
                     <Link to="/" className="logo footer__logo" aria-label="logo">
                         <SvgIcon id="ellipseMock" title="Logotype" />
@@ -18,7 +18,7 @@ export const FooterUI: React.FC = () => {
                     <p className="footer__copyright">
                         &copy; 2025 Центр современной драматургии Екатеринбург. Все права защищены.
                     </p>
-                    {/* <Social className="footer__social" /> */}
+                    {/* <Social className="footer__social-media" /> */}
                     <div className="footer__social-media">
                         <a href="#" className="footer__social-media-item">
                             <SvgIcon id="ellipseMock" title="VK icon" />
@@ -31,15 +31,15 @@ export const FooterUI: React.FC = () => {
 
                 <div className="footer__contacts">
                     <h3 className="footer__title">КОНТАКТЫ</h3>
-                    <p>Номер кассы</p>
+                    <p className="footer__contacts-text">Номер кассы</p>
                     <a href="tel:+71111111111" className="footer__phone">
                         +7(111)111-11-11
                     </a>
-                    <p>Другой номер</p>
+                    <p className="footer__contacts-text">Другой номер</p>
                     <a href="tel:+71111111111" className="footer__phone">
                         +7(111)111-11-11
                     </a>
-                    <p>Email</p>
+                    <p className="footer__contacts-text">Email</p>
                     <a href="mailto:dramcentr@gmail.com" className="footer__email">
                         dramcentr@gmail.com
                     </a>
@@ -47,10 +47,19 @@ export const FooterUI: React.FC = () => {
 
                 <div className="footer__schedule">
                     <h3 className="footer__title">ГРАФИК РАБОТЫ</h3>
-                    <p>Рабочие дни</p>
-                    <p className="footer__work-hours">Со вторника по воскресенье 9:00 — 21:00</p>
-                    <p>Выходные дни</p>
-                    <p className="footer__off-days">Понедельник</p>
+                    <div className="footer__work-hours-container">
+                        <p className="footer__work-days">Рабочие дни</p>
+                        <div className="footer__work-time">
+                            <span className="footer__work-time-text">Со вторника по воскресенье</span>
+                            <div className="footer__work-time-line">
+                                <span className="footer__work-time-start">9:00</span>
+                                <div className="footer__work-time-divider"></div>
+                                <span className="footer__work-time-end">21:00</span>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="footer__off-days">Выходные дни</p>
+                    <p className="footer__off-days-text">Понедельник</p>
                 </div>
             </div>
         </footer>
