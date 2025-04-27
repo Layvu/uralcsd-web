463793d4b9bbb17ffef2fe569afdca5c8d84e58c
koborochka <098mishawifi@gmail.com>
2025-04-27 17:55:59 +0500

    style(TeamCardFullUI): стилизация карточки участника

--- frontend/src/components/ui/TeamPage/TeamCardFull/TeamCardFullUI.tsx
+++ frontend/src/components/ui/TeamPage/TeamCardFull/TeamCardFullUI.tsx
@@ -1,37 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { TeamCardFullProps } from './type';
 import { DefaultBanner } from '@components/Shared/DefaultBanner';
 
+import './team-card-full.scss';
+import { marked } from 'marked';
+import DOMPurify from 'dompurify';
+
 export const TeamCardFullUI: React.FC<TeamCardFullProps> = React.memo(({ member, performancesWithRoles }) => {
-    if (!member){
-        return <div>Загрузка</div>;
-    };
+    const [biographyHtml, setBiographyHtml] = useState<string>('');
+
+    // без этого ругается на биографию
+    useEffect(() => {
+        const processBiography = async () => {
+            if (member?.biography) {
+                const rawHtml = await marked(member.biography);
+                const sanitizedHtml = DOMPurify.sanitize(rawHtml);
+                setBiographyHtml(sanitizedHtml);
+            }
+        };
+        processBiography();
+    }, [member?.biography]);
+
+    if (!member) {
+        return <div>Загрузка...</div>;
+    }
+
     return (
-        <section className='team-card-full'>
-            <div className='team-card-full__slider '>
-                <DefaultBanner 
-                    name={member.name} 
-                    images={member.images ? member.images.map((image: {url:string}) => 
-                        image.url ? image.url : '') : []}/>
+        <section className="team-card-full">
+            <div className="team-card-full__slider">
+                <DefaultBanner
+                    name={member.name}
+                    images={member.images ? member.images.map((image: { url: string }) => image.url || '') : []}
+                />
             </div>
-            {/* TODO Продюссеров еще надо рассмотреть */}
-            <div>{member.name}</div>
-            <div>{member.surname}</div>
-            <img src={member.mainPhoto?.url} />
 
-            {performancesWithRoles.map(({ performance, role }) =>
-                performance ? (
-                    <Link to={`/performances/${performance?.slug}`} key={performance?.id}>
-                        <div>
-                            {performance.title} - {role}
+            <div className="team-card-full__info wrap">
+                <div className="team-card-full__photo">
+                    <img src={member.mainPhoto?.url} alt={member.surname} />
+                </div>
+                <div className="team-card-full__info-container">
+                    <div className="team-card-full__name-container">
+                        <h2 className="team-card-full__name title-h2--underline">{member.name} {member.surname}</h2>
+                        <div className="team-card-full__position">{member.position}</div>
+                    </div>
+                    <div className="team-card-full__biography-container">
+                        <h3 className="title-h4">Биография</h3>
+                        <div className="team-card-full__biography" 
+                            dangerouslySetInnerHTML={{ __html: biographyHtml }}>
                         </div>
-                    </Link>
-                ) : (
-                    <div key={`no-performance-${member.id}-${role}`}>Спектакль не найден - {role}</div>
-                ),
-            )}
-
+                    </div> 
+                    <div className="team-card-full__spectacles-container">
+                        <h3 className="title-h4">Спектакли</h3>
+                        <div className="team-card-full__spectacles">
+                            {performancesWithRoles.map(({ performance, role }) =>
+                                performance ? (
+                                    <Link to={`/performances/${performance?.slug}`} key={performance?.id}>
+                                        <div className="team-card-full__spectacle-container">
+                                            <div className="team-card-full__spectacle-title">{performance.title}</div>
+                                            <div className="team-card-full__spectacle-role">{role}</div>
+                                            
+                                        </div>
+                                    </Link>
+                                ) : (
+                                    <div className="team-card-full__spectacle-container" key={`no-performance-${member.id}-${role}`}>
+                                        <div className="team-card-full__spectacle-title">Спектакль не найден </div>
+                                        <div className="team-card-full__spectacle-role">{role}</div>
+                                    </div>
+                                ),
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 });
