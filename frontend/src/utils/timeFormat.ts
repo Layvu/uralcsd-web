2d82eed4cfbbefa0b234aa1a2ff5697942215ec8
a.zemskii <zemskyalexey.writer@mail.ru>
2025-05-25 16:56:31 +0500

    chore: small build issues

--- frontend/src/utils/timeFormat.ts
+++ frontend/src/utils/timeFormat.ts
@@ -1,14 +1,17 @@
-import { format, parseISO } from 'date-fns';
+import { format } from 'date-fns';
 import { ru } from 'date-fns/locale';
+import { parseISO } from 'date-fns';
+// npm install date-fns@2.30.0 --save
+// npm install @types/date-fns --save-dev
 
-export const formatToWeekday = (dateString : string) => {
+export const formatToWeekday = (dateString: string) => {
     return format(parseISO(dateString), 'EEEE', { locale: ru });
 };
 
-export const formatDateTime = (dateString : string) => {
+export const formatDateTime = (dateString: string) => {
     return format(parseISO(dateString), 'd MMMM HH:mm', { locale: ru });
 };
 
-export const formatToMonth = (date : Date) => {
+export const formatToMonth = (date: Date) => {
     return date.toLocaleString('ru', { month: 'long', day: 'numeric' }).split(' ')[1];
 };
