516d4657ecac9413a4d8cc13dc7586f1a9e42ec7
koborochka <098mishawifi@gmail.com>
2025-06-02 19:42:12 +0500

    style(адаптив): адаптив для хедера, футера

--- frontend/src/components/ui/Footer/FooterUI.tsx
+++ frontend/src/components/ui/Footer/FooterUI.tsx
@@ -20,52 +20,57 @@ export const FooterUI: React.FC<FooterUIProps> = React.memo(({ contactsInfo }) =
                         &copy; 2025 Центр современной драматургии Екатеринбург. Все права защищены.
                     </p>
                 </div>
+                <div className="footer__info">
+                    <div className="footer__contacts">
+                        <h3 className="footer__title title-h5--underline">КОНТАКТЫ</h3>
+                        <ul className="footer__contacts-info-list">
+                            <li className="footer__contacts-info-item">
+                                <p className="footer__contacts-text">Номер кассы</p>
+                                <a href={`tel:${phones?.boxOffice?.replace(/[^\d+]/g, '')}`} className="footer__contacts-info-value">
+                                    {phones?.boxOffice}
+                                </a>
+                            </li>
+                            <li className="footer__contacts-info-item">
+                                <p className="footer__contacts-text">Основной телефон</p>
+                                <a href={`tel:${phones?.main?.replace(/[^\d+]/g, '')}`} className="footer__contacts-info-value">
+                                    {phones?.main}
+                                </a>
+                            </li>
+                            <li className="footer__contacts-info-item">
+                                <p className="footer__contacts-text">Email</p>
+                                <a href={`mailto:${email}`} className="footer__contacts-info-value">
+                                    {email}
+                                </a>
+                            </li>
+                            <li className="footer__contacts-info-item">
+                                <p className="footer__contacts-text">Адрес</p>
+                                <p className="footer__contacts-adress footer__contacts-info-value">
+                                    {address}
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
 
-                <div className="footer__contacts">
-                    <h3 className="footer__title title-h5--underline">КОНТАКТЫ</h3>
-                    <ul className="footer__contacts-info-list">
-                        <li className="footer__contacts-info-item">
-                            <p className="footer__contacts-text">Номер кассы</p>
-                            <a href={`tel:${phones?.boxOffice?.replace(/[^\d+]/g, '')}`} className="footer__contacts-info-value">
-                                {phones?.boxOffice}
-                            </a>
-                        </li>
-                        <li className="footer__contacts-info-item">
-                            <p className="footer__contacts-text">Основной телефон</p>
-                            <a href={`tel:${phones?.main?.replace(/[^\d+]/g, '')}`} className="footer__contacts-info-value">
-                                {phones?.main}
-                            </a>
-                        </li>
-                        <li className="footer__contacts-info-item">
-                            <p className="footer__contacts-text">Email</p>
-                            <a href={`mailto:${email}`} className="footer__contacts-info-value">
-                                {email}
-                            </a>
-                        </li>
-                        <li className="footer__contacts-info-item">
-                            <p className="footer__contacts-text">Адрес</p>
-                            <p className="footer__contacts-adress footer__contacts-info-value">
-                                {address}
-                            </p>
-                        </li>
-                    </ul>
-                </div>
-
-                <div className="footer__schedule">
-                    <h3 className="footer__title title-h5--underline">ГРАФИК РАБОТЫ</h3>
-                    <div className="footer__work-hours-container">
-                        <p className="footer__work-days">Рабочие дни</p>
-                        <div className="footer__work-time">
-                            <span className="footer__work-time-text">{workingDaysText}</span>
-                            <div className="footer__work-time-line">
-                                <span className="footer__work-time-start">{workingHours?.start}</span>
-                                <div className="footer__work-time-divider"></div>
-                                <span className="footer__work-time-end">{workingHours?.end}</span>
+                    <div className="footer__schedule">
+                        <h3 className="footer__title title-h5--underline">ГРАФИК РАБОТЫ</h3>
+                        <div className="footer__schedule-content-container">
+                            <div className="footer__work-hours-container">
+                                <p className="footer__work-days">Рабочие дни</p>
+                                <div className="footer__work-time">
+                                    <p className="footer__work-time-text">{workingDaysText}</p>
+                                    <div className="footer__work-time-line">
+                                        <p className="footer__work-time-start">{workingHours?.start}</p>
+                                        <div className="footer__work-time-divider"></div>
+                                        <p className="footer__work-time-end">{workingHours?.end}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="footer__off-days-container">
+                                <p className="footer__off-days">Выходные дни</p>
+                                <p className="footer__off-days-text">{daysOff?.join(', ')}</p>
                             </div>
                         </div>
                     </div>
-                    <p className="footer__off-days">Выходные дни</p>
-                    <p className="footer__off-days-text">{daysOff?.join(', ')}</p>
                 </div>
             </div>
         </footer>
