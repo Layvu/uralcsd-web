008da4eda7e15867659a82ab57ac1c0805602a22
a.zemskii <zemskyalexey.writer@mail.ru>
2025-07-28 19:53:27 +0300

    fix: dates display in UTC+5 timezone

--- frontend/src/utils/timeFormat.ts
+++ frontend/src/utils/timeFormat.ts
@@ -1,25 +1,30 @@
-import { format } from 'date-fns';
+import { format, parseISO } from 'date-fns';
 import { ru } from 'date-fns/locale';
-import { parseISO } from 'date-fns';
-// npm install date-fns@2.30.0 --save
-// npm install @types/date-fns --save-dev
+import { utcToZonedTime } from 'date-fns-tz';
+
+const yekaterinburgTimeZone = 'Asia/Yekaterinburg';
 
 export const formatToWeekday = (dateString: string) => {
-    return format(parseISO(dateString), 'EEEE', { locale: ru });
+    const zonedDate = utcToZonedTime(parseISO(dateString), yekaterinburgTimeZone);
+    return format(zonedDate, 'EEEE', { locale: ru });
 };
 
-export const formatToFullDateTime = (dateString : string) => {
-    return format(parseISO(dateString), 'd MMMM HH:mm', { locale: ru });
+export const formatToFullDateTime = (dateString: string) => {
+    const zonedDate = utcToZonedTime(parseISO(dateString), yekaterinburgTimeZone);
+    return format(zonedDate, 'd MMMM HH:mm', { locale: ru });
 };
 
 export const formatToMonth = (date: Date) => {
-    return date.toLocaleString('ru', { month: 'long', day: 'numeric' }).split(' ')[1];
+    const zonedDate = utcToZonedTime(date, yekaterinburgTimeZone);
+    return zonedDate.toLocaleString('ru', { month: 'long', day: 'numeric' }).split(' ')[1];
 };
 
-export const formatToDate = (dateString : string) => {
-    return format(parseISO(dateString), 'd MMMM', { locale: ru });
+export const formatToDate = (dateString: string) => {
+    const zonedDate = utcToZonedTime(parseISO(dateString), yekaterinburgTimeZone);
+    return format(zonedDate, 'd MMMM', { locale: ru });
 };
 
-export const formatToTime = (dateString : string) => {
-    return format(parseISO(dateString), 'HH:mm', { locale: ru });
+export const formatToTime = (dateString: string) => {
+    const zonedDate = utcToZonedTime(parseISO(dateString), yekaterinburgTimeZone);
+    return format(zonedDate, 'HH:mm', { locale: ru });
 };
